bf672aecff4b439f43c33bc35ea44d251606b296
William and Lady Navas <ladysantoyo@williams-mbp.uh.uhnj.org>
2019-03-07 11:18:24 -0500

    style-updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,42 +2,42 @@ var friends = [
     {
         name : "Ralphael",
         photo : "https://media.giphy.com/media/13DC3spQuglRFC/giphy.gif",
-        answers : [ 1,2,3,4,5],
+        answers : [ 1,2,3,4,5,2],
         score:[],
         number : []
     },
     {
         name : "Leonardo",
         photo : "https://media.giphy.com/media/Ala8Pjo4RN9kY/giphy.gif",
-        answers : [ 3,4,5,3,1],
+        answers : [ 3,4,5,3,1,3],
         score : [],
         number : []
     },
     {
         name : "Michaelangelo",
         photo : "https://media.giphy.com/media/8DFfcrGf6lWlG/giphy.gif",
-        answers : [ 4,5,3,2,1],
+        answers : [ 4,5,3,2,1,5],
         score : [],
         number : [] 
     },
     {
         name : "Donatello",
         photo : "https://media.giphy.com/media/cFdHXXm5GhJsc/giphy.gif",
-        answers : [ 1,3,3,4,1],
+        answers : [ 1,3,3,4,1,4],
         score : [],
         number : []
     },
     {
         name : "Mario",
         photo : "https://media.giphy.com/media/XNXbltfZQ0ofu/giphy.gif",
-        answers : [ 5,4,1,5,5],
+        answers : [ 5,4,1,5,5,3],
         score : [],
         number : []
     },
     {
         name : "Pikachu",
         photo : "https://media.giphy.com/media/U2nN0ridM4lXy/giphy.gif",
-        answers : [ 5,3,2,5,4],
+        answers : [ 5,3,2,5,4,1],
         score : [],
         number : []
     }
