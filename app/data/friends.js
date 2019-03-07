40335a716c65b11dda6d900a3456a01a4e11b1a8
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-03-06 23:08:35 -0500

    GET-method-POST

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,38 @@
 var friends = [
     {
         name : "Ralphael",
-        photo : "here",
+        photo : "https://media.giphy.com/media/13DC3spQuglRFC/giphy.gif",
         answers : [ 1,2,3,4,5],
-        score:[]
+        score:[],
+        number : []
     },
     {
-        name : "Leonard",
-        photo : "here",
+        name : "Leonardo",
+        photo : "https://media.giphy.com/media/Ala8Pjo4RN9kY/giphy.gif",
         answers : [ 3,4,5,3,1],
-        score : []
+        score : [],
+        number : []
     },
     {
-        name : "Michael Angelo",
-        photo : "here",
+        name : "Michaelangelo",
+        photo : "https://media.giphy.com/media/8DFfcrGf6lWlG/giphy.gif",
         answers : [ 4,5,3,2,1],
-        score : []
+        score : [],
+        number : [] 
     },
     {
         name : "Donatello",
-        photo : "here",
+        photo : "https://media.giphy.com/media/cFdHXXm5GhJsc/giphy.gif",
         answers : [ 1,3,3,4,1],
-        score : []
+        score : [],
+        number : []
+    },
+    {
+        name : "Mario",
+        photo : "https://media.giphy.com/media/XNXbltfZQ0ofu/giphy.gif",
+        answers : [ 5,4,1,5,5],
+        score : [],
+        number : []
     }
 
 ];
